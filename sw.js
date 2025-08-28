d139a089a41a15be9c44443c117cc0f88a72dd68
ItzFlipje <118839507+ItzFlipje@users.noreply.github.com>
2025-08-28 02:15:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+
+const cacheName = 'dino-runner-v1';
+const assets = [
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
